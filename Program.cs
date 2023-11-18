a31dcf4408314040e38a78329a9dbb67d33e3c45
nirzaf <mfmfazrin1986@gmail.com>
2023-11-18 11:36:23 +0300

    Update target framework and add logging
    The TargetFramework in QTS.EmailSender.csproj has been downgraded from net8.0 to net7.0. The Program.cs file has also been modified to include Microsoft.Extensions.DependencyInjection and Microsoft.Extensions.Logging, enabling console logging for better troubleshooting and visibility.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddLogging();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.AddConsole();
+    })
     .Build();
 
 host.Run();
